d571fd1d0f01d8253415587381feed1a93ccc651
Jari Sokka <sokkajar@gmail.com>
2022-01-13 17:39:36 +0200

    Add health check

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
   res.send('ok')
