6f694a22b70b768f30a8ee2a5368b63438b54ee4
Jari Sokka <sokkajar@gmail.com>
2022-01-13 16:54:41 +0200

    Slack error feature test

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
 app.get('/health', (req, res) => {
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('9') // change this string to ensure a new version deployed
+  res.send('10') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
